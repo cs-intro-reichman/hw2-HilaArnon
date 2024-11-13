268d6f12e7e2184539fcd352d872e3d03c82148a
HilaArnon <arnonhila@gmail.com>
2024-11-13 15:35:34 +0200

    Test 1

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,43 @@
 // Prints a crowd cheering output.
 public class Cheers {
+        /*
+         * Third question (15 points) 
+         * The Cheers program gets two inputs: a string and a non-negative integer value, and prints a crowd
+         * cheering output. Here is an example of the program’s execution:
+         * {'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'} the program prints "an", not "a"
+         * % java Cheers RUNI 5
+         * Give me an R: R!
+         * Give me a U: U!
+         * Give me an N: N!
+         * Give me an I: I!
+         * What does that spell?
+         * RUNI!!!
+         * RUNI!!!
+         * RUNI!!!
+         * RUNI!!!
+         * RUNI!!!
+         */
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            String word = args[0];
+            int times = Integer.parseInt(args[1]);
+            String special = "AEFHILMNORSX";
+            
+            int placeOfChar = 0;
+            while (placeOfChar <= word.length()){
+                char correntLetter = word.charAt(placeOfChar);
+                System.out.print("Give me ");                  
+                if (special.indexOf(correntLetter) >= 0){
+                        System.out.print("an ");
+                } else {
+                        System.out.print("a ");
+                }
+                System.out.println(correntLetter + ": " + correntLetter + "!");
+            }
+
+            System.out.println("What does that spell?");
+            int counter = 0;
+            while (counter < times){
+                System.out.println(word + "!!!");
+            }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,19 @@
 public class TimeCalc {
+    /*
+     * The TimeCalc program gets a time input which is given using the 24-hour hh:mm format, and a
+     * number of minutesToAdd input. The program computes time + minutesToAdd, and prints the
+     * resulting time using the 24-hour hh:mm format. For example, the time 10:30 + 4055 minutes
+     * happens to be 06:05. Here is one way to calculate this new time value:
+     * Total minutes = (10 hours * 60 minutes) + 30 minutes + 4055 minutes = 4685 minutes;
+     * Total hours = 4685 / 60 = 78;
+     * New hours = 78 % 24 = 6; // The remainder operator % is also known as modulo
+     * New minutes = 4685 – (78 * 60) = 5;
+     * Therefore the answer is 06:05.
+
+    */
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));   //from HW1
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4)); //from HW1
+        
     }
 }
