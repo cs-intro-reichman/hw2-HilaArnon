e5aa183ab67645f46fb3ac42388e6f0f5b57c10a
HilaArnon <arnonhila@gmail.com>
2024-11-14 08:26:24 +0200

    Test 3

--- TimeCalc.java
+++ TimeCalc.java
@@ -12,8 +12,51 @@ public class TimeCalc {
 
     */
     public static void main(String[] args) {
-        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));   //from HW1
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4)); //from HW1
+        int inputHours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));   //from HW1
+		int inputMinutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4)); //from HW1
+        int minutesToAdd = Integer.parseInt(args[1]);
         
+        int totalMinutes = (inputHours * 60) + inputMinutes + minutesToAdd;
+        int totalHours = totalMinutes / 60;
+        int hours = totalHours % 24;
+        int minutes = totalMinutes - (totalHours * 60);
+        /*int hours = inputHours + (minutesToAdd / 24);
+        System.out.println("hours " + hours);
+        System.out.println("minutesToAdd % 24" + minutesToAdd / 24);
+        int minutes = inputMinutes + (minutesToAdd - (hours * 60));
+        System.out.println("minutesToAdd - (hours * 60) " + (minutesToAdd - (hours * 60)));*/
+        if (minutes >= 60){
+            hours ++;
+            minutes -= 60;
+        }
+
+        // Checking 00:00
+        boolean check0 = false;		
+        if (hours == 0 && minutes == 0) { // 0:00
+			check0 = true;
+		}
+
+		// Checking the minutes
+		String newMinutes;
+		if (minutes < 10) {
+			newMinutes = "0" + String.valueOf(minutes);
+		} else {
+			newMinutes = String.valueOf(minutes);
+		}
+
+        // Checking the hours
+        String newhours;
+        if (hours < 10) {
+			newhours = "0" + String.valueOf(hours);
+		} else {
+			newhours = String.valueOf(hours);
+		}
+
+		// output
+		if (!check0) {
+			System.out.print(newhours + ":" + newMinutes);
+		} else { // 00:00
+			System.out.print("00:00");
+		}
     }
 }
