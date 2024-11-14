691d8d770585ec6e31018ad139458e39196f36b8
HilaArnon <arnonhila@gmail.com>
2024-11-14 21:49:03 +0200

    Test 6

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,30 @@
 // Computes an approximation of PI.
 public class CalcPi {
+	/* Fourth question (20 points)
+	 * % java CalcPi 10
+	 * pi according to Java: 3.141592653589793
+	 * pi, approximated: 3.0418396189294032
+	 * % java CalcPi 1000
+	 * pi according to Java: 3.141592653589793
+	 * pi, approximated: 3.140592653839794
+	 */
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int times = Integer.parseInt(args[0]);
+		int denominator = 1;
+		double countPI = 1.0;
+		while (times > 0){
+			denominator += 2;
+			//System.out.println("times: " + times);
+			//System.out.println("times % 2: " + times % 2);
+			//System.out.println("denominator: " + denominator);
+			if (times % 2 == 0){
+				countPI -= 1.0 / denominator;
+			} else {
+				countPI += 1.0 / denominator;
+			}
+			times --;
+		}
+		System.out.println("pi according to Java: 3.141592653589793");
+		System.out.println("pi, approximated: " + (countPI *4));
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -10,8 +10,8 @@ public class TimeCalc {
      * New hours = 78 % 24 = 6; // The remainder operator % is also known as modulo
      * New minutes = 4685 – (78 * 60) = 5;
      * Therefore the answer is 06:05.
-
     */
+    
     public static void main(String[] args) {
         int inputHours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));   //from HW1
 		int inputMinutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4)); //from HW1
