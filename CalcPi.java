b68ec4b55d282fe5f637fc048d80293760a711cf
HilaArnon <arnonhila@gmail.com>
2024-11-17 22:01:42 +0200

    Test 10

--- CalcPi.java
+++ CalcPi.java
@@ -24,7 +24,7 @@ public class CalcPi {
             times --;
         }
         System.out.println("pi according to Java: 3.141592653589793");
-        System.out.println("pi, approximated: " + (countPI *4.0));
+        System.out.print("pi, approximated: " + (countPI *4.0));
     }
 }
 
